5de9cd39e2fb2c1822e4972193144d7ab76e3a07
costalauro <costalauro@hotmail.com>
2021-03-09 18:39:35 -0300

    curso schemas aula 4

--- schemas/hospital2/src/hospital2/aula4.clj
+++ schemas/hospital2/src/hospital2/aula4.clj
@@ -7,12 +7,16 @@
 (def PosInt (s/pred pos-int? 'inteiro-positivo))
 (def Plano [s/Keyword])
 (def Paciente
-  {:id PosInt, :nome s/Str, :plano Plano})
+  {:idPosInt,
+   :nome                        s/Str,
+   :plano                       Plano
+   (s/optional-key :nascimento) s/Str})
 
 (pprint (s/validate Paciente {:id 15, :nome "lauro", :plano [:raio-x, :ultrasom]}))
 (pprint (s/validate Paciente {:id 15, :nome "lauro", :plano [:raio-x]}))
 (pprint (s/validate Paciente {:id 15, :nome "lauro", :plano []}))
 (pprint (s/validate Paciente {:id 15, :nome "lauro", :plano nil}))
+(pprint (s/validate Paciente {:id 15, :nome "lauro", :plano [], :nascimento "11/03/1996"}))
 
 
 
